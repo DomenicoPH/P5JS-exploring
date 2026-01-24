94851ae78996ddb62e8eec4bd3d08f5e12dc411d
Domenico Pagano <dpaganoh@gmail.com>
2026-01-24 13:17:23 -0500

    ascii art

---
+++ project001-asciiart-01/monalisa.js
@@ -0,0 +1,39 @@
+// Mona Lisa Test 01
+
+let img;
+let size;
+let asciiChar = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+
+function preload() {
+  //img = loadImage("img/cats.png");
+  img = loadImage("img/test.jpg");
+}
+
+function setup() {
+  createCanvas(400, 400);
+  img.resize(80,0);
+  size = width / img.width;
+  print(size);
+  print(asciiChar);
+}
+
+function draw() {
+  background(220);
+  
+  //image(img, 0, 0);
+
+    for(let i=0; i < img.width; i++){
+        for(let j=0; j < img.height; j++){
+            let pixelVal = img.get(i, j);
+            let c = brightness(pixelVal);
+            let tIndex = floor(map(c, 0, 100, 0, asciiChar.length));
+            
+            let x = i * size + size/2;
+            let y = j * size + size/2;
+            let t = asciiChar.charAt(tIndex);
+            textSize(size);
+            textAlign(CENTER, CENTER);
+            text(t, x, y);
+        }
+    }
+}
---
+++ project001-asciiart-01/sketch.js
@@ -0,0 +1,53 @@
+// Mona Lisa Test 02
+
+let img;
+let size;
+let asciiChar = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+let video;
+let vidw = 64;
+let vidh = 48;
+let scl = 10;
+let charw;
+let charh;
+
+function preload() {
+  //img = loadImage("img/cats.png");
+  img = loadImage("img/test.jpg");
+}
+
+function setup() {
+  createCanvas(vidw * scl, vidh * scl);
+  //img.resize(100, 0);
+  //size = width / img.width;
+
+  video = createCapture(VIDEO);
+  video.size(vidw,vidh);
+  charw = width / video.width;
+  charh = height / video.height;
+}
+
+function draw() {
+  background(220);
+  video.loadPixels();
+  //print(img.pixels.length);
+
+  for (let i = 0; i < video.width; i++) {
+    for (let j = 0; j < video.height; j++) {
+      let pixelIndex = (i + j * video.width) * 4;
+      let r = video.pixels[pixelIndex + 0];
+      let g = video.pixels[pixelIndex + 1];
+      let b = video.pixels[pixelIndex + 2];
+
+      let bright = brightness(color(r, g, b));
+      let tIndex = floor(map(bright, 0, 100, 0, asciiChar.length));
+
+      let x = i * charw + charw / 2;
+      let y = j * charh + charh / 2;
+      let t = asciiChar.charAt(tIndex);
+      textSize(charw);
+      textAlign(CENTER, CENTER);
+      text(t, x, y);
+    }
+  }
+
+}
