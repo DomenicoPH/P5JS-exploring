e0b868b662be2094e42d3876d51d477f18a73c82
Domenico Pagano <dpaganoh@gmail.com>
2026-01-27 10:19:25 -0500

    angled lines 2

---
+++ patterns/angled-lines-2/sketch.js
@@ -0,0 +1,37 @@
+let rez = 0.02;
+let t = 0;
+
+function setup() {
+  createCanvas(600,600);
+  let saveButton = createButton("save jpg");
+  saveButton.position(0,0);
+  saveButton.mousePressed(saveArt);
+  noLoop();
+}
+
+function draw() {
+  background(255);
+  space = 20;
+  size = 20;
+
+  for (i = 0; i < width; i += space) {
+    for (j = 0; j < height; j += space) {
+      n = noise(i * rez, j * rez, t);
+      //n = random(1);
+      //stroke(n*255,n*255,n*255,255);
+      stroke(0);
+      //fill(n*255,n*255,n*255,255);
+      strokeWeight(3);
+      if (n < 0.5) {
+        line(i, j, i + size, j + size);
+      } else {
+        line(i + size, j, i, j + size);
+      }
+    }
+    t += 0.00002;
+  }
+}
+
+function saveArt() {
+  save("canvas.jpg");
+}
